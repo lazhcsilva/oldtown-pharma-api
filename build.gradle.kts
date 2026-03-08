e1b9f294dad96a180eaaeda0244b1bad6942a3ae
Lázaro Silva <lazarosilva@caern.local>
2026-03-07 21:50:54 -0300

    Start project

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "br.com.oldtown.pharma"
+version = "0.0.1-SNAPSHOT"
+description = "oldtown-pharma-api"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/br/com/oldtown/pharma/OldtownPharmaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.oldtown.pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldtownPharmaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldtownPharmaApiApplication.class, args);
+    }
+
+}
