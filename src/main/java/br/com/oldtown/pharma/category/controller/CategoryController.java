d067478d39e51ad0da401a52ee33b957f8edcc77
Lázaro Silva <lazarosilva@caern.local>
2026-04-07 20:37:34 -0300

    Update Category request

--- src/main/java/br/com/oldtown/pharma/category/controller/CategoryController.java
+++ src/main/java/br/com/oldtown/pharma/category/controller/CategoryController.java
@@ -10,13 +10,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Categories", description = "Operations related to categories")
 @RestController
@@ -44,7 +43,8 @@ public class CategoryController {
     @Operation(summary = "Get category by name")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Category retrieved successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid request")
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
     })
     @GetMapping("/search")
     public ResponseEntity<CategoryResponse> findByName(
@@ -54,20 +54,33 @@ public class CategoryController {
     }
 
     @Operation(summary = "Post a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+    })
     @PostMapping
     public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CreateCategoryRequest category) {
         return ResponseEntity.ok(categoryService.create(category));
     }
 
     @Operation(summary = "Update category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<CategoryResponse> update(@PathVariable Long id, @Valid @RequestBody UpdateCategoryRequest category) {
+    public ResponseEntity<CategoryResponse> update(@PathVariable @Positive Long id,
+                                                   @Valid @RequestBody UpdateCategoryRequest category) {
         return ResponseEntity.ok(categoryService.update(id, category));
     }
 
     @Operation(summary = "Delete category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable @Positive Long id) {
         categoryService.delete(id);
         return ResponseEntity.noContent().build();
     }
