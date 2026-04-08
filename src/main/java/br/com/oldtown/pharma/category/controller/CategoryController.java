089233908bac29727045cb95dd020ef73a049b8a
Lázaro Silva <lazarosilva@caern.local>
2026-04-08 00:20:13 -0300

    Fix category validations

--- src/main/java/br/com/oldtown/pharma/category/controller/CategoryController.java
+++ src/main/java/br/com/oldtown/pharma/category/controller/CategoryController.java
@@ -14,12 +14,15 @@ import jakarta.validation.constraints.Positive;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Categories", description = "Operations related to categories")
 @RestController
 @RequestMapping("/api/v1/categories")
+@Validated
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -30,14 +33,25 @@ public class CategoryController {
 
     @Operation(summary = "Get all categories")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Products retrieved successfully"),
+            @ApiResponse(responseCode = "200", description = "Categories retrieved successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid request")
     })
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<PagedModel<CategoryResponse>> findAll(
             @Parameter(hidden = true) Pageable pageable) {
         Page<CategoryResponse> page = categoryService.getAll(pageable);
         return ResponseEntity.ok(new PagedModel<>(page));
+    }
+
+    @Operation(summary = "Get category by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category retrieved successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> findById(@Valid @Positive Long id) {
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
     @Operation(summary = "Get category by name")
@@ -48,36 +62,37 @@ public class CategoryController {
     })
     @GetMapping("/search")
     public ResponseEntity<CategoryResponse> findByName(
-            @Parameter(description = "Category", example = "free")
+            @Parameter(description = "Category name", example = "Medicines")
             @RequestParam String name) {
         return ResponseEntity.ok(categoryService.findByName(name));
     }
 
     @Operation(summary = "Post a new category")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Success"),
+            @ApiResponse(responseCode = "201", description = "Category created successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "409", description = "Category already exists")
     })
     @PostMapping
-    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CreateCategoryRequest category) {
-        return ResponseEntity.ok(categoryService.create(category));
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CreateCategoryRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(request));
     }
 
-    @Operation(summary = "Update category")
+    @Operation(summary = "Update a category")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "200", description = "Category updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
     })
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponse> update(@PathVariable @Positive Long id,
-                                                   @Valid @RequestBody UpdateCategoryRequest category) {
-        return ResponseEntity.ok(categoryService.update(id, category));
+                                                   @Valid @RequestBody UpdateCategoryRequest request) {
+        return ResponseEntity.ok(categoryService.update(id, request));
     }
 
-    @Operation(summary = "Delete category")
+    @Operation(summary = "Delete a category")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "204", description = "Category deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
     })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable @Positive Long id) {
