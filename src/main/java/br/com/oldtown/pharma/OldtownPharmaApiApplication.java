942702d3e94467e10362549981cede9d1cd53f98
Lázaro Silva <lazarosilva@caern.local>
2026-04-22 00:39:08 -0300

    Add ConfigurationPropertiesScan annotation

--- src/main/java/br/com/oldtown/pharma/OldtownPharmaApiApplication.java
+++ src/main/java/br/com/oldtown/pharma/OldtownPharmaApiApplication.java
@@ -2,8 +2,10 @@ package br.com.oldtown.pharma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OldtownPharmaApiApplication {
 
     public static void main(String[] args) {
