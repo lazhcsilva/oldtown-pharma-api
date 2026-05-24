89bbac04a91c044c884dcbc1e4443e08bc490508
Lázaro Silva <lazarosilva@caern.local>
2026-05-23 21:10:36 -0300

    Add mapstruct implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,14 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
     implementation("org.springframework.boot:spring-boot-starter-validation:4.0.4")
     implementation("org.springframework.security:spring-security-core:7.0.3")
+    implementation("org.mapstruct:mapstruct:1.6.3")
+
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
     runtimeOnly("com.h2database:h2")
 }
 
